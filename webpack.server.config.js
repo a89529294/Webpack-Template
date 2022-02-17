26a9441effc2541134b85408fe8dd0679ab10765
Albert Chang <a89529294@hotmail.com>
2022-02-17 13:43:09 +0800

    update one webpack 5 syntax

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -9,7 +9,7 @@ const config = {
     path: path.resolve(__dirname, "dist"),
     filename: "backend-bundle.js",
   },
-  target: "node", // in order to ignore built-in modules like path, fs, etc.
+  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()],
   // in order to ignore all modules in node_modules folder
   // The server already has access to this folder so there is no need to bundle them. When we
